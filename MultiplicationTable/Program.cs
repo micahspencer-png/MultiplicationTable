a764eeec1c4143034f48f8bc4ff09ff336b6f6e2
micahspencer-png <micahspencer@isu.edu>
2025-09-17 08:11:39 -0600

    Set up multiplication table

--- MultiplicationTable/Program.cs
+++ MultiplicationTable/Program.cs
@@ -1,10 +1,27 @@
 ﻿namespace MultiplicationTable
 {
+    //Micah Spencer
+    //RCET2265
+    //Fall 2025
+    //Multiplication Table Program
+    //
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string userInput;
+            int number = 12;
+            Console.WriteLine("Type in What Multiplication Table You Wish to See");
+            userInput = Console.ReadLine();
+            number = int.Parse(userInput);
+
+            for (int i = 0; i < number; i++)
+            {
+
+            }
+
+            //pause
+            Console.Read();
         }
     }
 }
